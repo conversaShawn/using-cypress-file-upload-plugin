ed4e2456d92d7fd65910d1f837ae80e8d1cfda1a
conversaShawn <shawn.harris3510@gmail.com>
2021-10-14 00:41:48 -0700

    successfully moved config to new path

--- edtell-srs-testing-suite/frontend/integration/catalog/catalogSyllabusTests_spec.js
+++ edtell-srs-testing-suite/frontend/integration/catalog/catalogSyllabusTests_spec.js
@@ -2,19 +2,19 @@
 /// <reference types="cypress" />
 
 // type definitions for custom commands like "createDefaultTodos"
-/// <reference types="../support" />
+//  <reference types="../../support" />
 
-describe("Testing Angies code", () => {
+describe("Testing upload pdf", () => {
     before("visit download url", () => {
       cy.visit('https://www.docdroid.net/');
     });
     it("Add Syllabus", () => {
       cy.get("#select").contains("Select Files").should("exist");
-      // be sure to update "fixturesFolder": "../../../fixtures" in cypress.json
-      cy.fixture("moveSample.pdf").then((fileContent) => {
+  
+      cy.fixture("2018-How-the-World-Tests.pdf.pdf").then((fileContent) => {
         cy.get("input[type=file]").attachFile({
           fileContent: fileContent.toString(),
-          fileName: "moveSample.pdf",
+          fileName: "2018-How-the-World-Tests.pdf.pdf",
           mimeType: "application/pdf",
         });
       });
